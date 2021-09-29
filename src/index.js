a069fb0b1c602f95109b88bd5f57735838848792
Kyounguk Chae <wolseo1@gmail.com>
2021-09-29 23:23:52 +0900

    feat: Add 2nd additional improvements
    Bold the currently selected item in the move list.

--- src/index.js
+++ src/index.js
@@ -98,7 +98,10 @@ class Game extends React.Component {
 				'Go to game start';
 			return (
 				<li key={move}>
-					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+					<button
+						onClick={() => this.jumpTo(move)}
+						style={this.state.stepNumber === move ? {fontWeight:'bold'} : {}}
+					>{desc}</button>
 				</li>
 			);
 		});
